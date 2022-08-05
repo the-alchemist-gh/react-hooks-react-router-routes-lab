692505d86c2639581f22042375b5813e704469bc
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-08-05 15:27:47 +0000

    Completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+      <h1>Movies Page</h1>
+      {
+        movies.map((movie, index)=>(
+          <div key={index}>
+            <h3>{movie.title} | <small>{movie.time} mins</small></h3>
+            <ul>
+              {
+                movie.genres.map((genre,index)=>(<li key={index} >{genre}</li>))
+              }
+            </ul>
+          </div>
+        ))
+      }
+    </div>;
 }
 
 export default Movies;
